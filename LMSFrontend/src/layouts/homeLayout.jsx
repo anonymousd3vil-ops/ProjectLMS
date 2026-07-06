160fbebc2fa648a0bc4425b0db61a8e034d79bd3
anonymousd3vil-ops <anonymous.d3vil.07@gmail.com>
2026-07-06 19:31:53 +0530

    Added logout/signup buttons on the sidebar

--- LMSFrontend/src/layouts/homeLayout.jsx
+++ LMSFrontend/src/layouts/homeLayout.jsx
@@ -1,16 +1,36 @@
 import { FiMenu } from "react-icons/fi";
 import { AiFillCloseCircle } from "react-icons/ai";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import {useSelector, useDispatch } from 'react-redux'
 
 import Footer from '../components/footer/footer.jsx'
 
 function HomeLayout({children}) {
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+
+  const isLoggedIn = useSelector((state) => state?.auth?.isLoggedIn);
+
+  const role = useSelector((state) => state?.auth?.role);
+
   const hideDrawer = () => {
     const drawer = document.getElementById("my-drawer");
     if (drawer) {
       drawer.checked = false;
     }
   };
+
+  const handelLogout = async (event) => {
+    event.preventDefault();
+
+    const res = await dispatch(logout());
+    
+    if(res?.payload?.success){
+      navigate('/')
+    }
+  }
 
   return (
     <div className="min-h-screen">
@@ -27,17 +47,26 @@ function HomeLayout({children}) {
           <label htmlFor="my-drawer" className="drawer-overlay"></label>
           <ul className="menu p-4 w-48 sm:w-80 min-h-full bg-base-100 text-base-content relative ">
             <li className="w-fit absolute right-2 z-50">
-              <button onClick={hideDrawer} className="btn btn-ghost btn-sm">
+              <button onClick={hideDrawer} className="btn btn-ghost btn-sm  hover:bg-yellow-600">
                 <AiFillCloseCircle size={24} />
               </button>
             </li>
 
-            <div className="mt-10">
               <li className="hover:bg-yellow-500 rounded-lg transition-all ease-in-out duration-300">
                 <Link to="/" onClick={hideDrawer}>
                   Home
                 </Link>
               </li>
+              
+              {
+                isLoggedIn && role === 'ADMIN' && (
+                  <li className="hover:bg-yellow-500 rounded-lg transition-all ease-in-out duration-300">
+                    <Link to="/admin/dashboard">
+                      Admin Dashboard
+                    </Link>
+                  </li>
+                )
+              }
 
               <li className="hover:bg-yellow-500 rounded-lg transition-all ease-in-out duration-300">
                 <Link to="/courses" onClick={hideDrawer}>
@@ -56,7 +85,39 @@ function HomeLayout({children}) {
                   About Us
                 </Link>
               </li>
-            </div>
+              
+              {
+                !isLoggedIn && (
+                    <div className="w-full flex flex-col md:flex-row items-center gap-5 justify-center my-2">
+                      <Link to='/login' className="w-1/2">
+                        <button className="btn-primary px-4 py-1 w-full cursor-pointer hover:bg-yellow-600 font-semibold rounded-md bg-yellow-500 transition-all ease-in-out duration-500"> 
+                          Login
+                        </button>
+                      </Link>
+                      <Link to='/signup' className="w-1/2">
+                        <button className="btn-secondary px-4 py-1 cursor-pointer font-semibold rounded-md w-full border border-yellow-500 hover:bg-yellow-500 transition-all ease-in-out  duration-500"> 
+                          SignUp
+                        </button>
+                      </Link>
+                    </div>
+                )
+              }
+              {
+                isLoggedIn && (
+                    <div className="w-full flex flex-col md:flex-row items-center gap-5 justify-center my-2">
+                      <Link to='/user/profile' className="w-1/2">
+                        <button className="btn-primary px-4 py-1 w-full cursor-pointer hover:bg-yellow-600 font-semibold rounded-md bg-yellow-500 transition-all ease-in-out duration-500"> 
+                          Profile
+                        </button>
+                      </Link>
+                      <Link className="w-1/2">
+                        <button onClick={handelLogout} className="btn-secondary px-4 py-1 cursor-pointer font-semibold rounded-md w-full border border-yellow-500 hover:bg-yellow-500 transition-all ease-in-out  duration-500"> 
+                          Logout
+                        </button>
+                      </Link>
+                    </div>
+                )
+              }
           </ul>
         </div>
       </div>
