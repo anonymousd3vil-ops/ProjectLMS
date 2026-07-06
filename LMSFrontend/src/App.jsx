4c7394494365bd2e72b17f3032fd3642bc1482df
anonymousd3vil-ops <anonymous.d3vil.07@gmail.com>
2026-07-07 00:04:45 +0530

    Added not Found page

--- LMSFrontend/src/App.jsx
+++ LMSFrontend/src/App.jsx
@@ -2,6 +2,7 @@ import './styles/style.css'
 import HomePage from './pages/homePage.jsx'
 import { Route, Routes } from 'react-router-dom'
 import AboutUs from './pages/aboutus.jsx'
+import NotFound from './pages/notFound.jsx'
 
 
 function App() {
@@ -9,9 +10,10 @@ function App() {
   return (
     <>
       <Routes>
-
         <Route path='/' element={<HomePage/>}> </Route>
         <Route path='/about' element={<AboutUs />}> </Route> 
+
+        <Route path='*' element={<NotFound />}></Route>
       </Routes>
     </>
   )
---
+++ LMSFrontend/src/pages/notFound.jsx
@@ -0,0 +1,48 @@
+import { useNavigate } from "react-router-dom";
+// import { Home, ArrowLeft } from "lucide-react";
+
+function NotFound() {
+    const navigate = useNavigate();
+
+    return (
+        <div className="min-h-screen bg-linear-to-br from-[#020716] via-[#081122] to-[#0d1b36] flex items-center justify-center px-4">
+            <title>Not Found</title>
+            <div className="max-w-3xl text-center">
+
+                <h1 className="text-8xl md:text-[10rem] font-black bg-linear-to-r from-[#FFF7AE] via-[#FFD700] to-[#B8860B] bg-clip-text text-transparent select-none drop-shadow-[0_0_25px_rgba(255,215,0,0.35)]">
+                    404
+                </h1>
+
+                {/* Heading */}
+                <h2 className="mt-4 text-3xl md:text-5xl font-bold text-white">
+                    Lost in Space?
+                </h2>
+
+                <p className="mt-5 text-base md:text-lg text-gray-400 leading-relaxed">
+                    The page you're looking for doesn't exist or has been
+                    moved to another galaxy. Let's get you back on track.
+                </p>
+
+                <div className="flex flex-col md:flex-row items-center justify-center gap-4 m-10">
+
+                    <button
+                        onClick={() => navigate("/")}
+                        className="bg-yellow-500 font-bold text-xl hover:bg-yellow-600 btn rounded-lg btn-wide transition-all ease-in-out duration-300"
+                    >
+                        Go Home
+                    </button>
+
+                    <button
+                        onClick={() => navigate(-1)}
+                        className="btn border border-yellow-500 font-bold text-xl hover:bg-yellow-500 btn-wide text-white transition-all ease-in-out duration-300"
+                    >
+                        Previous Page
+                    </button>
+
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default NotFound;    
